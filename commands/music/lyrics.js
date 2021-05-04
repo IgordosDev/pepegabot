e77fd74349a901bf4489b0e7be98ead026a2de30
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 21:38:50 +0500

    ну и че сколько ещё попыток будет

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -3,9 +3,9 @@ module.exports = [{
       code: `$log[lyrics от $userTag: $songInfo[title]]
       $title[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];title;название песни не найдено];;$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];links.#RIGHT#0#LEFT#genius;null]]
 $description[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];lyrics;для этого трека текст не найден.]]
-$thumbnail[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];thumbnail.#RIGHT#0#LEFT#genius;]]
 $suppressErrors[произошли технические шоколадки, попробуй потом]
 $color[RANDOM]
+$botTyping
 $onlyIf[$voiceID!=;]
 $argsCheck[0;]
       `}, {
@@ -13,9 +13,9 @@ name: "lyrics",
 code:`$log[lyrics от $userTag: $message]
 $title[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];title;название песни не найдено];$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];links.#RIGHT#0#LEFT#genius;null]]
 $description[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];lyrics;текст для трека не найден]]
-$thumbnail[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];thumbnail.#RIGHT#0#LEFT#genius;]]
 $suppressErrors[какая-то ошибка, попробуй потом]
 $color[RANDOM]
+$botTyping
 $onlyIf[$message!=;]
 `
 }]
