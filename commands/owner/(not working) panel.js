80ce3acd49f98e42a699ed4551bc5fc79472e1d2
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 10:19:00 +0500

    перенос бота с techhost на heroku

---
+++ commands/owner/(not working) panel.js
@@ -0,0 +1,13 @@
+//module.exports = {
+      name: "panel",
+      aliases:['dashboard'],
+      code: `$log[panel от $userTag: $message]
+      $sendDM[$mentioned[1];{title:http://fn1-new.techhost.cc}{field:Логин:$username[$mentioned[1]]:yes}{field:Пароль:$randomString[8]:yes}]
+      $djsEval[client.Express.addAccounts({
+username: "$username[$mentioned[1]]", //any username <3
+password: "$randomString[8]" //Make sure its secure!
+})]
+      $reply[$messageID;Написал данные в лс;yes]
+      $onlyIf[$mentioned[1]!=;Пингани кого-нибудь <:Pepega:779683581844521000><a:Clap:787284235152130118>]
+      $onlyForIDs[694849711437250581;Только для <@!694849711437250581>. <:Okayge:816271601162649610>]`
+}
---
+++ commands/owner/prefix.js
@@ -0,0 +1,7 @@
+module.exports = ({
+    name: "prefix",
+    code: `$log[Prefix set on "$noMentionMessage[1]" by $userTag]
+    Префикс установлен на \`$noMentionMessage[1]\`.
+    $onlyForIDs[694849711437250581;лол нет]
+    `
+    });
