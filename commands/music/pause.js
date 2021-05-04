07a5eec642f5893da05ffcbbfc59137e16d7426a
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 21:11:36 +0500

    смена переменной на временный объект

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -2,14 +2,15 @@ module.exports = ({
     name: "pause",
     aliases: ['resume'],
     code: `$log[Pause от $userTag]
-    $if[$getServerVar[pause]==0]
-    $setServerVar[pause;1]
+    $if[$getObjectProperty[pause]==0]
+    $addObjectProperty[pause;1]
     $pauseSong
     Плеер поставлен на паузу <:Okayge:816271601162649610><a:TeaTime:782163966168137728>
     $else
-    $setServerVar[pause;0]
+    $addObjectProperty[pause;0]
     $resumeSong
     Плеер снят с паузы <a:pepegaJAM:753708890188808393>
     $endif
-    `
-    });
+    $addObjectProperty[pause;0]
+    $createObject[{}]
+    `});
