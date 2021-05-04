ca504bda003879cd229af085451a6c87543d18d8
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 20:48:27 +0500

    а, ну ещё добавил обложку трека с ссылкой

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -1,8 +1,9 @@
 module.exports = [{
       name: "lyrics",
       code: `$log[lyrics от $userTag: $songInfo[title]]
-      $title[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];title;название песни не найдено]]
+      $title[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];title;название песни не найдено];;$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];links.#RIGHT#0#LEFT#genius;null]]
 $description[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];lyrics;для этого трека текст не найден.]]
+$thumbnail[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];thumbnail.#RIGHT#0#LEFT#genius;undefined]
 $suppressErrors[произошли технические шоколадки, попробуй потом]
 $color[RANDOM]
 $onlyIf[$voiceID!=;]
@@ -10,8 +11,9 @@ $argsCheck[0;]
       `}, {
 name: "lyrics",
 code:`$log[lyrics от $userTag: $message]
-$title[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];title;название песни не найдено]] 
+$title[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];title;название песни не найдено];$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];links.#RIGHT#0#LEFT#genius;null]]
 $description[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];lyrics;текст для трека не найден]]
+$thumbnail[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];thumbnail.#RIGHT#0#LEFT#genius;undefined]
 $suppressErrors[какая-то ошибка, попробуй потом]
 $color[RANDOM]
 $onlyIf[$message!=;]
