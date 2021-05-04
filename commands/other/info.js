9e6d738bb9f9ef59d5ca3ad62b01d87b13177931
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 10:25:32 +0500

    Исправление ram со старого хоста

--- commands/other/info.js
+++ commands/other/info.js
@@ -4,7 +4,7 @@ module.exports = {
       code: `$log[info от $userTag]
 $color[RANDOM]
       $title[Техническая инфа]
-      $description[CPU - $cpu%\nCPU модель: $djsEval[require ('os').cpus()[0].model;yes]\nПлатформа: $djsEval[require ('os').platform();yes]\nRAM - $ram MB/$maxRam MB (на панели указано 500 MB)\nBot ping - \`$botPing\` ms\nWebsocket - \`$ping\` ms\nAoi.js - $packageVersion\nNodeJS - $getObjectProperty[nodev]\nDiscord.js - $djsEval[d.object.nodev = process.version
+      $description[CPU - $cpu%\nCPU модель: $djsEval[require ('os').cpus()[0].model;yes]\nПлатформа: $djsEval[require ('os').platform();yes]\nRAM - $ram MB/$maxRam MB\nBot ping - \`$botPing\` ms\nWebsocket - \`$ping\` ms\nAoi.js - $packageVersion\nNodeJS - $getObjectProperty[nodev]\nDiscord.js - $djsEval[d.object.nodev = process.version
 d.object.discordv = require('discord.js').version;yes]] $footer[Аптайм: $uptime]
 $addField[#️⃣ $channelCount;⌨️ $channelCount[text]\n🔊 $channelCount[voice];yes]
 $addField[👥 $membersCount;🟢  $membersCount[$guildID;online]\n⛔ $membersCount[$guildID;dnd]\n🌙 $membersCount[$guildID;idle]\n⚫ $membersCount[$guildID;offline]\n🤖 $botCount;yes]
