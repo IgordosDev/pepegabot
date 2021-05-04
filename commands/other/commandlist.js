1c499e92c32112d24fd5990c220831f9b682ebee
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 21:25:05 +0500

    Update commandlist.js

--- commands/other/commandlist.js
+++ commands/other/commandlist.js
@@ -3,7 +3,7 @@ module.exports = {
       code: `$log[commandlist от $userTag: $noMentionMessage[1]]
       $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;Error]]
       $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;Error]\n\`\`\`]
-      $addField[Usage;$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;Error];yes]
+      $addField[Usage;\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;Error]\n\`\`\`;yes]
       $color[GREEN]
       $onlyIf[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;400]!=400;{color:RED} {title:Ошибка} {description:Либо апи сдохло, либо какая-то ошибка(скорее всего функция не найдена) $customEmoji[WYE]}]
       $botTyping
