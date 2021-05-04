ae43174d705cd855fd7a653abf67dbfe7b0569ce
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 20:41:06 +0500

    фикс повторения команды

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -1,16 +1,16 @@
 module.exports = [{
       name: "lyrics",
       code: `$log[lyrics от $userTag: $songInfo[title]]
-      $title[$songInfo[title]] 
+      $title[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];title;название песни не найдено]]
 $description[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$songInfo[title]; ; %20;-1];lyrics;для этого трека текст не найден.]]
 $suppressErrors[произошли технические шоколадки, попробуй потом]
 $color[RANDOM]
 $onlyIf[$voiceID!=;]
-$onlyIf[$message==;]
+$argsCheck[0;]
       `}, {
 name: "lyrics",
 code:`$log[lyrics от $userTag: $message]
-$title[$message] 
+$title[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];title;название песни не найдено]] 
 $description[$jsonRequest[https://some-random-api.ml/lyrics?title=$replaceText[$message; ; %20;-1];lyrics;текст для трека не найден]]
 $suppressErrors[какая-то ошибка, попробуй потом]
 $color[RANDOM]
