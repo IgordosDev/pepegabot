7c32b102806db42411a222cc3235f58483b57750
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 21:24:24 +0500

    фикс yt-search

--- commands/other/yt-search.js
+++ commands/other/yt-search.js
@@ -1,8 +1,8 @@
 module.exports = ({
     name: "ytsearch",
     aliases: ['yts', 'yt-search', 'youtube'],
-    code: `
-$replaceText[$jsonRequest[http://api.somecool.repl.co/yt-search?search=$replaceText[$message; ;%20;-1];url;хз ошибка какая-то, url нету];youtube.com/watch?v=;youtu.be/;1]
-$onlyIf[$jsonRequest[http://api.somecool.repl.co/yt-search?search=$replaceText[$message; ;%20;-1];message;всё ок]==всё ок;Запрос не найден]
+    code: `$log[yt-search от $userTag: $noMentionMessage]
+$replaceText[$jsonRequest[http://api.somecool.repl.co/yt-search?search=$replaceText[$noMentionMessage; ;%20;-1];url;Запрос не найден];youtube.com/watch?v=;youtu.be/;1]
+$botTyping
 $argsCheck[>1;Введи запрос для поиска на ютубе]
 `});
