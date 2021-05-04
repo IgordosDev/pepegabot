4819a2d1a80394eda94a5d28daf52e3059192e49
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-04 21:54:17 +0500

    откат

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -2,15 +2,13 @@ module.exports = ({
     name: "pause",
     aliases: ['resume'],
     code: `$log[Pause от $userTag]
-    $if[$getObjectProperty[pause]==0]
-    $addObjectProperty[pause;1]
+    $if[$getServerVar[pause]==0]
+    $setServerVar[pause;1]
     $pauseSong
     Плеер поставлен на паузу <:Okayge:816271601162649610><a:TeaTime:782163966168137728>
     $else
-    $addObjectProperty[pause;0]
+    $setServerVar[pause;0]
     $resumeSong
     Плеер снят с паузы <a:pepegaJAM:753708890188808393>
     $endif
-    $addObjectProperty[pause;0]
-    $createObject[{}]
     `});
