48793d519b3db61980bdb575c816d210d2e4ce76
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-06 00:20:32 +0500

    Update and rename commands/owner/role.js to commands/other/role.js

--- commands/other/role.js
+++ commands/other/role.js
@@ -8,6 +8,6 @@ $addObjectProperty[role;$findRole[$getObjectProperty[name]]]
 $createRole[$getObjectProperty[name];$noMentionMessage[1];$sub[$rolePosition[815925339711864862];1]]
 $addObjectProperty[name;$replaceText[$noMentionMessage;$noMentionMessage[1] $noMentionMessage[2];;1]]
 $createObject[{}]
-$argsCheck[>4;Для создания роли и выдачи её кому-то, используйте \`\`\`\n$getServerVar[prefix]role HEX-цвет @Юзер Имя роли\n\`\`\`Например: \`\`\`\n$getServerVar[prefix]role $getRoleColor[$highestRole] @$username Имя.\n\`\`\`]
+$argsCheck[>3;Для создания роли и выдачи её кому-то, используйте \`\`\`\n$getServerVar[prefix]role HEX-цвет @Юзер Имя роли\n\`\`\`Например: \`\`\`\n$getServerVar[prefix]role $getRoleColor[$highestRole] @$username Имя.\n\`\`\`]
 $onlyIf[$hasRoles[$authorID;821236913725243412]==true;Только для модеров]
 `})
