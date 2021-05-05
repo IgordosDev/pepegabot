5aa0a373154701b68d53f5ff84bc073427f5bfc2
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-05 22:41:39 +0500

    Перепись команды

--- commands/other/commandlist.js
+++ commands/other/commandlist.js
@@ -1,10 +1,15 @@
 module.exports = {
       name: "commandlist",
       code: `$log[commandlist от $userTag: $noMentionMessage[1]]
-      $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;Error]]
-      $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;Error]\n\`\`\`]
-      $addField[Usage;\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;Error]\n\`\`\`;yes]
-      $color[GREEN]
-      $onlyIf[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;]!=400;{color:RED} {title:$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;] $jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;statusText;]} {description:$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;message;]}]
-      $botTyping
+      $if[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;]!=200]
+     $color[RED]
+     $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;] $jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;statusText;]]
+     $description:$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;message;]}]
+      $else
+     $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;Error]]
+     $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;Error]\n\`\`\`]
+     $addField[Usage:;\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;Error]\n\`\`\`;yes]
+     $color[GREEN]
+     $endif
+     $botTyping
       `}
