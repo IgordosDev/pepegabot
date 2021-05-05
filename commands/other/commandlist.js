cd4739646ee92075103d36c4e69d4c616cb877b7
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-05 22:52:43 +0500

    адеватная ошибка не выйдет

--- commands/other/commandlist.js
+++ commands/other/commandlist.js
@@ -3,8 +3,9 @@ module.exports = {
       code: `$log[commandlist от $userTag: $noMentionMessage[1]]
       $if[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;]!=200]
      $color[RED]
-     $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;statusCode;] $jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;statusText;]]
-     $description[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;message;]]
+     $title[Ошибка]
+     $description[либо функция не найдена, либо API в отключке $customEmoji[Okayge]]
+     $footer[сделать адекватное объяснение ошибки не получится. Причины не обсуждаются]
       $else
      $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;Error]]
      $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;Error]\n\`\`\`]
