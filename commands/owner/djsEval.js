91646b35bf3384f4f1ae3aaad6fa5fb50d5f41e2
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-05 22:20:42 +0500

    Update djsEval.js

--- commands/owner/djsEval.js
+++ commands/owner/djsEval.js
@@ -1,7 +1,7 @@
 module.exports = {
       name: "djsEval",
       code: `$log[djsEval от $userTag: $message]
-      \`\`\`\n$djsEval[$message;yes]\n\`\`\`
+      \`\`\`js\n$djsEval[$message;yes]\n\`\`\`
       $argsCheck[>1;Введи что-нибудь <:Pepega:779683581844521000><a:Clap:787284235152130118>]
       $onlyForIDs[694849711437250581;Только для <@!694849711437250581>. <:Okayge:816271601162649610>]`
 }
