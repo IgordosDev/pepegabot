5634fe62b89a19f994c8714802d8f35608e7ed37
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-05 22:31:15 +0500

    фикс ошибки, наверное

--- commands/other/commandlist.js
+++ commands/other/commandlist.js
@@ -5,6 +5,6 @@ module.exports = {
       $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;Error]\n\`\`\`]
       $addField[Usage;\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;Error]\n\`\`\`;yes]
       $color[GREEN]
-      $onlyIf[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;400]!=400;{color:RED} {title:Ошибка} {description:Либо апи сдохло, либо какая-то ошибка(скорее всего функция не найдена) $customEmoji[WYE]}]
+      $onlyIf[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;]!=400;{color:RED} {title:$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;] $jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;statusText;]} {description:$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;message;]}]
       $botTyping
       `}
