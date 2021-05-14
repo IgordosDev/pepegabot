b3fda3b83361c4970c62cea29c71d6db402deb1c
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-14 17:58:20 +0500

    Убрал ошибки из реквестов

--- commands/other/chatbot.js
+++ commands/other/chatbot.js
@@ -1,9 +1,9 @@
 module.exports = [{
   name: '$alwaysExecute',
-  code: `$log[Chatbot reply "$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$replaceText[$noMentionMessage; ;%20;-1]&botname=Sophia&ownername=Igordos&user=$replaceText[$username; ;%20;-1];message;]" to "$replaceText[$noMentionMessage; ;%20;-1]" by $userTag]
-$channelSendMessage[753673183298846730;{title:Chatbot reply} {field:Запрашиваемый текст для API:\`\`\`fix\n$replaceText[$getObjectProperty[user_translate];%20; ;-1]\n\`\`\`:yes} {field:Оригинальное сообщение:\`\`\`fix\n$noMentionMessage\`\`\`:yes} {field:Переведённый ответ API:\`\`\`\n$getObjectProperty[chatbot_translate]\n\`\`\`:yes} {field:Оригинальный ответ API:\`\`\`fix\n$replaceText[$getObjectProperty[chatbot_reply];%20; ;-1]\n\`\`\`:yes} {footer:$userTag • $authorID:$authorAvatar};no]
+  code: `$log[Chatbot reply "$replaceText[$ame=Sophia&ownername=Igordos&user=$replaceText[$username; ;%20;-1];message;]" to "$replaceText[$getObjectProperty[chatbot_reply];%20; ;-1]" by $userTag]
+$channelSendMessage[753673183298846730;{color:RED} {title:Chatbot reply} {field:Запрашиваемый текст для API:\`\`\`fix\n$replaceText[$getObjectProperty[user_translate];%20; ;-1]\n\`\`\`:yes} {field:Оригинальное сообщение:\`\`\`fix\n$noMentionMessage\`\`\`:yes} {field:Переведённый ответ API:\`\`\`\n$getObjectProperty[chatbot_translate]\n\`\`\`:yes} {field:Оригинальный ответ API:\`\`\`fix\n$replaceText[$getObjectProperty[chatbot_reply];%20; ;-1]\n\`\`\`:yes} {footer:$userTag • $authorID:$authorAvatar};no]
 $sendWebhook[$getServerVar[webhook_id];$getServerVar[webhook_token];$getObjectProperty[chatbot_translate] {author:$username:$authorAvatar} {description:$message} {color:$getRoleColor[$highestRole]}]
-$addObjectProperty[chatbot_translate;$jsonRequest[https://normal-api.ml/translate?text=$getObjectProperty[chatbot_reply]&to=ru;translated;Произошла ошибка в переводе ❌]]
+$addObjectProperty[chatbot_translate;$jsonRequest[https://normal-api.ml/translate?text=$getObjectProperty[chatbot_reply]&to=ru;translated;]]
 $addObjectProperty[chatbot_reply;$replaceText[$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$getObjectProperty[user_translate]&botname=Sophia&ownername=Igordos&user=$getObjectProperty[username];message;]; ;%20;-1]]
 $onlyIf[$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$getObjectProperty[user_translate]&botname=Sophia&ownername=Igordos&user=$getObjectProperty[username];message;]!=;]
 $addObjectProperty[username;$replaceText[$username; ;%20;-1]]
@@ -25,7 +25,7 @@ $sendWebhook[$getServerVar[webhook_id];$getServerVar[webhook_token];Chatbot mode
 $else
 $log[chatbot on by $userTag]
 $setServerVar[chatbot;on]
-$sendWebhook[$splitText[1];$splitText[2];Chatbot mode ✅]
+$sendWebhook[$splitText[1];$splitText[2];Chatbot mode ✅\nПереводы могут быть неверны]
 $setServerVar[chatbot_channel;$channelID]
 $setServerVar[webhook_token;$splitText[2]]
 $setServerVar[webhook_id;$splitText[1]]
