b71a9c49adf2b156d9be1027bac4e75021150e1e
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-23 17:53:07 +0500

    Pepoga

--- commands/other/info.js
+++ commands/other/info.js
@@ -5,13 +5,13 @@ module.exports = {
 $color[RANDOM]
       $author[Техническая инфа]
 $title[${require ('os').hostname}]
-$addField[CPU;$cpu%\nCPU модель: $djsEval[require ('os').cpus()[0].model;yes];yes]
-$addField[Платформа;${require ('os').type} $djsEval[require('os').arch;yes];yes]
-$addField[RAM;$ram MB/$maxRam MB;yes]
-$addField[🏓 Пинг;\`$botPing\` ms\nWebsocket - \`$ping\` ms;yes]
 $addField[Библиотеки;Aoi.js - $packageVersion\nNodeJS - $getObjectProperty[nodev]\nDiscord.js - $djsEval[d.object.nodev = process.version
 d.object.discordv = require('discord.js').version;yes];yes]
+$addField[🏓 Пинг;\`$botPing\` ms\nWebsocket - \`$ping\` ms;yes]
+$addField[RAM;$ram MB/$maxRam MB;yes]
+$addField[Платформа;${require ('os').type} $djsEval[require('os').arch;yes];yes]
+$addField[CPU;$cpu%\nCPU модель: $djsEval[require ('os').cpus()[0].model;yes];yes]
 $footer[Дизайн украден у бота $userTag[736963755904335942], но слегка переделан | Сервер запущен;$userAvatar[736963755904335942]]
-$addTimestamp[${Date.now() - require('os').uptime() * 1000}]
+$addTimestamp[${try Date.now() - require('os').uptime() * 1000}]
 $description[#️⃣ $channelCount: ⌨️ $channelCount[text]; 🔊 $channelCount[voice]\n👥 $membersCount: 🟢  $membersCount[$guildID;online]; ⛔ $membersCount[$guildID;dnd]; 🌙 $membersCount[$guildID;idle]; ⚫ $membersCount[$guildID;offline]; 🤖 $botCount]
 `}
