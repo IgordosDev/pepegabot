6817c19a4840a30dd8203f4636ba9d99b9a91d84
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-05-24 17:17:04 +0500

    вы всё ещё уёбки

--- commands/other/info.js
+++ commands/other/info.js
@@ -4,7 +4,7 @@ module.exports = {
       code: `$log[info от $userTag]
 $color[RANDOM]
       $author[Техническая инфа]
-$title[${require ('os').hostname}]
+$title[Сервер: ${require ('os').hostname}]
 $addField[Библиотеки;Aoi.js - $packageVersion\nNodeJS - $getObjectProperty[nodev]\nDiscord.js - $djsEval[d.object.nodev = process.version
 d.object.discordv = require('discord.js').version;yes];yes]
 $addField[🏓 Пинг;\`$botPing\` ms\nWebsocket - \`$ping\` ms;yes]
@@ -13,5 +13,4 @@ $addField[Платформа;${require ('os').type} $djsEval[require('os').arch;
 $addField[CPU;$cpu%\nCPU модель: $djsEval[require ('os').cpus()[0].model;yes];yes]
 $footer[Дизайн украден у бота $userTag[736963755904335942], но слегка переделан | Бот запущен;$userAvatar[736963755904335942]]
 $addTimestamp[${Date.now() - process.uptime().toFixed(0) * 1000}]
-$description[#️⃣ $channelCount: ⌨️ $channelCount[text]; 🔊 $channelCount[voice]\n👥 $membersCount: 🟢  $membersCount[$guildID;online]; ⛔ $membersCount[$guildID;dnd]; 🌙 $membersCount[$guildID;idle]; ⚫ $membersCount[$guildID;offline]; 🤖 $botCount]
 `}
