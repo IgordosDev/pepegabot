9b338a6ead5ffc83a8637495a6425be9087c3358
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-06-10 23:42:10 +0500

    Update info.js

--- commands/other/info.js
+++ commands/other/info.js
@@ -4,13 +4,15 @@ module.exports = {
       code: `$log[info от $userTag]
 $color[RANDOM]
       $author[Техническая инфа]
-$title[Сервер: ${require ('os').hostname}]
-$addField[Библиотеки;Aoi.js - $packageVersion\nNodeJS - $getObjectProperty[nodev]\nDiscord.js - $djsEval[d.object.nodev = process.version
+      $title[Сервер: ${require ('os').hostname}]
+      $thumbnail[$userAvatar[$clientID]]
+$addField[Библиотеки(и нода);Aoi.js - $packageVersion\nNodeJS - $getObjectProperty[nodev]\nDiscord.js - $djsEval[d.object.nodev = process.version
 d.object.discordv = require('discord.js').version;yes];yes]
 $addField[🏓 Пинг;\`$botPing\` ms\nWebsocket - \`$ping\` ms;yes]
 $addField[RAM;$ram MB/$maxRam MB;yes]
 $addField[Платформа;${require ('os').type} $djsEval[require('os').arch;yes];yes]
 $addField[CPU;$cpu%\nCPU модель: $djsEval[require ('os').cpus()[0].model;yes];yes]
-$footer[Дизайн украден у бота $userTag[736963755904335942], но слегка переделан | Бот запущен;$userAvatar[736963755904335942]]
+$addField[Инфо бота;Серверов − $serverCount\nЮзеров − $membersCount;no]
+$footer[Пепега: $userTag[$botOwnerID] | Бот запущен;$userAvatar[$botOwnerID]]
 $addTimestamp[${Date.now() - process.uptime().toFixed(0) * 1000}]
 `}
