aaa498808aee80aeb2cad36b2438290681561bd7
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-06-11 02:28:38 +0500

    Update commandlist.js

--- commands/other/commandlist.js
+++ commands/other/commandlist.js
@@ -1,14 +1,16 @@
 module.exports = {
       name: "commandlist",
       code: `$log[commandlist от $userTag: $noMentionMessage[1]]
-      $if[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;statusText;https://sun9-37.userapi.com/impg/SuZlHrMLzMLrs1Z_EPTVxciSZ9db_I591Ix-MQ/j_vBAIIyBFI.jpg?size=937x720&quality=96&sign=0eba1fd70f1dd5577de9ae378db44cc9&type=album]==OK]
-$title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;]]
+      $if[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;]!=200]
+     $color[RED]
+     $title[Ошибка]
+     $description[либо функция не найдена, либо API в отключке $customEmoji[Okayge]]
+     $footer[сделать адекватное объяснение ошибки не получится. Причины не обсуждаются]
+      $else
+     $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;]]
      $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;]\n\`\`\`]
      $addField[Usage:;\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;]\n\`\`\`;yes]
      $color[GREEN]
-$else
-      ошибка по-другому не работает
      $endif
-     $argsCheck[>1;Введи функцию какую-нибудь мб]
      $botTyping
       `}
