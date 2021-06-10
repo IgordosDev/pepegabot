7dfcf4b0e94e73cc9170fb74777a01893d4a597a
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-06-11 02:05:46 +0500

    Update serverinfo.js

--- commands/other/serverinfo.js
+++ commands/other/serverinfo.js
@@ -6,6 +6,6 @@ name: "serverinfo",
   $thumbnail[$serverIcon]
   $addField[Кол-во эмоутов;$emoteCount]
   $addField[#️⃣ Каналов: $channelCount;⌨️ Текстовых: $channelCount[text]\n🔊 Войсов: $channelCount[voice];yes]
-  $addField[👥 Участников: $membersCount/$maximumMembers;🟢 Онлайн: $membersCount[$guildID;online]\n⛔ DnD: $membersCount[$guildID;dnd]\n🌙 Не активны: $membersCount[$guildID;idle]\n⚫ $membersCount[$guildID;offline]\n🤖 $botCount $else $endif;yes]
+  $addField[👥 Участников: $membersCount/$maximumMembers;🟢 Онлайн: $membersCount[$guildID;online]\n⛔ DnD: $membersCount[$guildID;dnd]\n🌙 Не активны: $membersCount[$guildID;idle]\n⚫ $membersCount[$guildID;offline]\n🤖 $botCount;yes]
   $footer[Пепега сервера: $userTag[$ownerID] | Сервер создан;$userAvatar[$ownerID]]
-$addTimestamp[${Date.now() - message.guild.createdTimestamp().toFixed(0) * 1000}]`});
+$addTimestamp[${Date.now() - message.guild.createdTimestamp.toFixed(0) * 1000}]`});
