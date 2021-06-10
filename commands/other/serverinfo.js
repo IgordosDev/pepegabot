68765ebcdbd3837a80e8145b88c235d93d985700
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-06-11 02:37:11 +0500

    Update serverinfo.js

--- commands/other/serverinfo.js
+++ commands/other/serverinfo.js
@@ -3,10 +3,12 @@ name: "serverinfo",
   aliases: ['server-info', 'server', 'сервер'],
   code: `$author[Инфо о сервере]
   $title[$serverName;https://discord.com/channels/$guildID]
-  $thumbnail[$serverIcon]
+  $if[$serverIcon!=null] $thumbnail[$serverIcon] $else $endif
   $if[$serverBoostCount!=0] $description[Бустов: $serverBoostCount] $else $endif
-  $addField[Кол-во эмоутов;$emoteCount]
   $addField[#️⃣ Каналов: $channelCount;⌨️ Текстовых: $channelCount[text]\n🔊 Войсов: $channelCount[voice];yes]
-  $addField[👥 Участников: $membersCount/$maximumMembers;🟢 Онлайн: $membersCount[$guildID;online]\n⛔ DnD: $membersCount[$guildID;dnd]\n🌙 Не активны: $membersCount[$guildID;idle]\n⚫ $membersCount[$guildID;offline]\n🤖 $botCount;yes]
+  $addField[👥 Участников: $membersCount/$maximumMembers;🟢 Онлайн: $membersCount[$guildID;online]\n⛔ DnD: $membersCount[$guildID;dnd]\n🌙 Не активны: $membersCount[$guildID;idle]\n⚫ Оффлайн: $membersCount[$guildID;offline]\n🤖 Ботов: $botCount;yes]
   $footer[Пепега сервера: $userTag[$ownerID] | Сервер создан;$userAvatar[$ownerID]]
+  $color[$if[$getObjectProperty[highserverrole]!=#000000] $getObjectProperty[highserverrole] $else RANDOM $endif]
+  $addObjectProperty[$getRoleColor[$highestServerRole];highserverrole]
+  $createObject[{}]
 $addTimestamp[$djsEval[message.guild.createdTimestamp;yes]]`});
