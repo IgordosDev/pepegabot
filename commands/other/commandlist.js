8d579d08d2ad000ad880953450c6331a12cb2852
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-06-11 01:57:51 +0500

    Update commandlist.js

--- commands/other/commandlist.js
+++ commands/other/commandlist.js
@@ -1,7 +1,7 @@
 module.exports = {
       name: "commandlist",
       code: `$log[commandlist от $userTag: $noMentionMessage[1]]
-$title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;{title:Ошибка} {description:Заданная функция не найдена. (*а мб апи сдохло*)} {color:RED}]]
+$title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;https://sun9-37.userapi.com/impg/SuZlHrMLzMLrs1Z_EPTVxciSZ9db_I591Ix-MQ/j_vBAIIyBFI.jpg?size=937x720&quality=96&sign=0eba1fd70f1dd5577de9ae378db44cc9&type=album]]
      $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;]\n\`\`\`]
      $addField[Usage:;\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;]\n\`\`\`;yes]
      $color[GREEN]
