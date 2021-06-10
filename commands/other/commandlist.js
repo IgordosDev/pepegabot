49b827d6a561cf5dfd5eb1fc85a4059bd50ab372
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-06-11 00:03:27 +0500

    Update commandlist.js

--- commands/other/commandlist.js
+++ commands/other/commandlist.js
@@ -1,13 +1,7 @@
 module.exports = {
       name: "commandlist",
       code: `$log[commandlist от $userTag: $noMentionMessage[1]]
-      $if[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;status;]!=200]
-     $color[RED]
-     $title[Ошибка]
-     $description[либо функция не найдена, либо API в отключке $customEmoji[Okayge]]
-     $footer[сделать адекватное объяснение ошибки не получится. Причины не обсуждаются]
-      $else
-     $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;]]
+$title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;{title:Ошибка} {description:Заданная функция не найдена. (*а мб апи сдохло*)} {color:RED}]]
      $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;]\n\`\`\`]
      $addField[Usage:;\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;]\n\`\`\`;yes]
      $color[GREEN]
