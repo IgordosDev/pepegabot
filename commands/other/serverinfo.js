bee22e72ec9de71112b3c4d34bbc25c9563cad87
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-06-11 01:25:09 +0500

    Update serverinfo.js

--- commands/other/serverinfo.js
+++ commands/other/serverinfo.js
@@ -4,9 +4,8 @@ name: "serverinfo",
   code: `$author[Инфо о сервере]
   $title[$serverName;https://discord.com/channels/$guildID]
   $thumbnail[$serverIcon]
-  $if[$serverBoostCount!=0] $description[Бустов: $serverBoostCount] $else $endif
   $addField[Кол-во эмоутов;$emoteCount]
-  $addField[#️⃣ Каналов: $channelCount;⌨️ Текстовых: $channelCount[text] $if[$channelCount[voice]!=0]\n🔊 Войсов: $channelCount[voice] $else $endif;yes]
+  $addField[#️⃣ Каналов: $channelCount;⌨️ Текстовых: $channelCount[text]\n🔊 Войсов: $channelCount[voice];yes]
   $addField[👥 Участников: $membersCount/$maximumMembers;🟢 Онлайн: $membersCount[$guildID;online]\n⛔ DnD: $membersCount[$guildID;dnd]\n🌙 Не активны: $membersCount[$guildID;idle]\n⚫ $membersCount[$guildID;offline]\n🤖 $botCount $else $endif;yes]
   $footer[Пепега сервера: $userTag[$ownerID] | Сервер создан;$userAvatar[$ownerID]]
 $addTimestamp[${Date.now() - message.guild.createdTimestamp().toFixed(0) * 1000}]`});
