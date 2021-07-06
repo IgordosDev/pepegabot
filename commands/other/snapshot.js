178d063a20a948bda72a3582efb1f4f2219800de
Igordos <igordos@national.shitposting.agency>
2021-07-06 16:06:32 +0500

    удалил токен апишки

--- commands/other/snapshot.js
+++ commands/other/snapshot.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 module.exports = {
       name: "snapshot",
       code: `$log[Snapshot от $userTag: $message[1]]
-      $if[$jsonRequest[https://api.apiflash.com/v1/urltoimage?access_key=c98a99e7c6cd4f068c8f942e96b5b642&format=png&height=1024&response_type=json&url=$message[1]&width=1280;url;Error]==Error]
-      Произошла какая-то ошибка, пойди чекни https://api.apiflash.com/v1/urltoimage?access_key=c98a99e7c6cd4f068c8f942e96b5b642&format=png&height=1024&response_type=json&url=$message[1]&width=1280
+      $if[$jsonRequest[https://api.apiflash.com/v1/urltoimage?access_key=${process.env.SNAPSHOT}&format=png&height=1024&response_type=json&url=$message[1]&width=1280;url;Error]==Error]
+      Произошла какая-то ошибка, пойди чекни https://api.apiflash.com/v1/urltoimage?access_key=${process.env.SNAPSHOT}&format=png&height=1024&response_type=json&url=$message[1]&width=1280
       $else
-      $title[Снапшот <:Okayge:816271601162649610>;$jsonRequest[https://api.apiflash.com/v1/urltoimage?access_key=c98a99e7c6cd4f068c8f942e96b5b642&format=png&height=1024&response_type=json&url=$message[1]&width=1280;url;]]
-      $image[$jsonRequest[https://api.apiflash.com/v1/urltoimage?access_key=c98a99e7c6cd4f068c8f942e96b5b642&format=png&height=1024&response_type=json&url=$message[1]&width=1280;url;]]
+      $title[Снапшот <:Okayge:816271601162649610>;$jsonRequest[https://api.apiflash.com/v1/urltoimage?access_key=${process.env.SNAPSHOT}&format=png&height=1024&response_type=json&url=$message[1]&width=1280;url;]]
+      $image[$jsonRequest[https://api.apiflash.com/v1/urltoimage?access_key=${process.env.SNAPSHOT}&format=png&height=1024&response_type=json&url=$message[1]&width=1280;url;]]
       $footer[Вызвано $username;$authorAvatar]
 $addTimestamp
 $color[RANDOM]
