48c9d64dd36cdc4d5a7a15c33538e975c0913b5c
Igordos <igordos@national.shitposting.agency>
2021-10-30 23:50:04 +0500

    Update reboot.js

--- commands/owner/reboot.js
+++ commands/owner/reboot.js
@@ -3,7 +3,7 @@ module.exports = {
 aliases:['перезапуск', 'перезагрузка', 'reset', 'reload', 'restart', 'рестарт'],
       code: `
       $reboot
-      $reply[$messageID;$randomText[понял, ребутаюсь <:OMEGALULiguess:797922459180466186>;https://tenor.com/view/change-da-world-my-final-message-goodbye-rat-gif-15029442;https://tenor.com/view/thanos-snap-rip-thanos-avengers-endgame-gif-14134497];no]
+      $reply[$messageID;$randomText[<:OMEGALULiguess:797922459180466186>;https://tenor.com/view/change-da-world-my-final-message-goodbye-rat-gif-15029442;https://tenor.com/view/thanos-snap-rip-thanos-avengers-endgame-gif-14134497];no]
       $log[reboot от $userTag]
-      $onlyForIDs[694849711437250581;Только для <@!694849711437250581>. <:Okayge:816271601162649610>]`
+      $onlyForIDs[694849711437250581;<@!694849711437250581> дмитрий нагиев вызывает]`
 }
