c1659dbc3bde99c20e96a242a37e37ed100f8258
Igordos <igordos@national.shitposting.agency>
2021-10-31 02:33:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -35,10 +35,8 @@ bot.loadCommands(`./commands/`) //Allows Commands executed by `commands` folder
 bot.readyCommand({ //проверка на наличие новой версии библиотеки
   channel: "753673183298846730",
   code: `<@!$botOwnerID>, доступна новая версия библиотеки aoi.js \`$jsonRequest[https://api.leref.ga/package/version;version;]\`, желательно поменять её в **package.json** или вырезать эту строчку об напоминании
-  $onlyIf[$jsonRequest[https://api.leref.ga/package/version;version;$packageVersion]>$packageVersion;]
+  $onlyIf[$jsonRequest[https://api.leref.ga/package/version;version;$packageVersion]!=$packageVersion;]
   $onlyIf[$jsonRequest[https://api.leref.ga/package/version;status;]==200;Сделать запрос на наличие новой версии библиотеки не удалось.]
-$else
-$endif
 `})
 bot.command({
 name: "ping", 
