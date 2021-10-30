afca6caa9d8cda8ff742dbe5c75015ed3aa0c1db
Igordos <igordos@national.shitposting.agency>
2021-10-31 00:19:33 +0500

    Update commandlist.js

--- commands/other/commandlist.js
+++ commands/other/commandlist.js
@@ -1,10 +1,10 @@
 module.exports = {
       name: "commandlist",
-      code: `$log[commandlist от $userTag: $noMentionMessage[1]]
-$title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;];$replaceText[https://aoi.leref.ga/functions/$toLowercase[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;];$;usd;1]]]
+      code: `
+     $title[$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;];$toLowercase[$replaceText[https://aoi.leref.ga/functions/$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.name;];$;usd;1]]]
      $description[\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.desc;]\n\`\`\`]
      $addField[Usage:;\`\`\`\n$jsonRequest[https://api.leref.ga/functions/$noMentionMessage[1]?limit=1&page=1;data#RIGHT#0#LEFT#.usage;]\n\`\`\`;yes]
      $color[GREEN]
-     $argsCheck[>1;Введи функцию мб]
+     $argsCheck[>1;Введи функцию]
      $botTyping
-      `}
+     `}
