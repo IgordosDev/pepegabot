9104065d89aa5ddd53c9ebccfe00ac82255185fe
Igordos <igordos@national.shitposting.agency>
2021-11-04 11:33:17 +0500

    санчерн

--- commands/music/play.js
+++ commands/music/play.js
@@ -4,15 +4,15 @@ module.exports = {
       code: `
     $if[$checkContains[$noMentionMessage[1];youtu.be;youtube.com]==true]
     **$username** добавил в очередь **$playSong[$noMentionMessage[1];2m;yes;Произошла какая-то ошибка]**
-    $else
-    **$username** добавил в очередь **$playSong[$noMentionMessage;2m;yes;Произошла какая-то ошибка]**
     $elseIf[$checkContains[$noMentionMessage[1];spotify;]==true]
     **$username** добавил в очередь **$playSpotify[$noMentionMessage[1];name;no;Произошла ошибка]**
     $elseIf[$checkContains[$noMentionMessage[1];soundcloud;]==true]
     **$username** добавил в очередь **$playSoundCloud[$noMentionMessage[1];${process.env.SOUNDCLOUD};2m;yes;no;Произошла ошибка при воспроизведении]
+    $elseIf[$hasLink[$noMentionMessage[1]]==false]
+    **$username** добавил в очередь **$playSong[$noMentionMessage;2m;yes;Произошла какая-то ошибка]**
     $endelseIf
     $endif
     $argsCheck[>1;Введи название песни, или ссылку на неё]
     $onlyIf[$voiceID!=;Может сначала ты зайдёшь в войс]
     `
-}  /*7: If[$hasLink[$noMentionMessage[1]]==false] */
+} 
