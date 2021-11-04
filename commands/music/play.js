571a0ee429fc6d11751712a5595c60722b834534
Igordos <igordos@national.shitposting.agency>
2021-11-04 11:45:53 +0500

    злодей ужасный герой отважный

--- commands/music/play.js
+++ commands/music/play.js
@@ -6,8 +6,10 @@ module.exports = {
     **$username** добавил в очередь **$playSong[$noMentionMessage[1];2m;yes;Произошла какая-то ошибка]**
     $elseIf[$checkContains[$noMentionMessage[1];spotify;]==true]
     **$username** добавил в очередь **$playSpotify[$noMentionMessage[1];name;no;Произошла ошибка]**
+    $endelseIf
     $elseIf[$checkContains[$noMentionMessage[1];soundcloud;]==true]
     **$username** добавил в очередь **$playSoundCloud[$noMentionMessage[1];${process.env.SOUNDCLOUD};2m;yes;no;Произошла ошибка при воспроизведении]
+    $endelseIf
     $elseIf[$hasLink[$noMentionMessage[1]]==false]
     **$username** добавил в очередь **$playSong[$noMentionMessage;2m;yes;Произошла какая-то ошибка]**
     $endelseIf
@@ -15,4 +17,4 @@ module.exports = {
     $argsCheck[>1;Введи название песни, или ссылку на неё]
     $onlyIf[$voiceID!=;Может сначала ты зайдёшь в войс]
     `
-} 
+}
