474a68ba1b8489c877bfbe79af0d876707298a04
Igordos <igordos@national.shitposting.agency>
2021-11-04 11:21:49 +0500

    $if: Invalid Usage: missing $endif

--- commands/music/play.js
+++ commands/music/play.js
@@ -8,9 +8,10 @@ module.exports = {
     **$username** добавил в очередь **$playSpotify[$noMentionMessage[1];name;no;Произошла ошибка]**
     $elseIf[$checkContains[$noMentionMessage[1];soundcloud;]==true]
     **$username** добавил в очередь **$playSoundCloud[$noMentionMessage[1];${process.env.SOUNDCLOUD};2m;yes;no;Произошла ошибка при воспроизведении]
-    $elseIf[$hasLink[$noMentionMessage[1]]==false]
-    **$username** добавил в очередь **$playSong[$noMentionMessage[1];2m;yes;Произошла какая-то ошибка]**
+    $else /*If[$hasLink[$noMentionMessage[1]]==false] */
+    **$username** добавил в очередь **$playSong[$noMentionMessage;2m;yes;Произошла какая-то ошибка]**
     $endelseIf
+    $endif
     $argsCheck[>1;Введи название песни, или ссылку на неё]
     $onlyIf[$voiceID!=;Может сначала ты зайдёшь в войс]
     `
