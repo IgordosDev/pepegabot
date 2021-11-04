768b82976122e4e9a0b8666040efcc44723097cd
Igordos <igordos@national.shitposting.agency>
2021-11-04 13:11:50 +0500

    откат

--- commands/music/play.js
+++ commands/music/play.js
@@ -2,6 +2,10 @@ module.exports = {
       name: "play",
       aliases:['p', 'плей', 'playSong'],
       code: `
+    **$username** добавил в очередь **$playSong[$noMentionMessage;2m;yes;Произошла какая-то ошибка]**
+    $argsCheck[>1;Введи название песни, или ссылку на неё]
+    $onlyIf[$voiceID!=;Может сначала ты зайдёшь в войс]
+    `} /*
     $if[$checkContains[$noMentionMessage[1];youtu.be;youtube.com]==true]
     **$username** добавил в очередь **$playSong[$noMentionMessage[1];2m;yes;Произошла какая-то ошибка]**
     $else
@@ -12,6 +16,4 @@ module.exports = {
     **$username** добавил в очередь **$playSoundCloud[$noMentionMessage[1];${process.env.SOUNDCLOUD};2m;yes;no;Произошла ошибка при воспроизведении]
     $endelseIf
     $endif
-    $argsCheck[>1;Введи название песни, или ссылку на неё]
-    $onlyIf[$voiceID!=;Может сначала ты зайдёшь в войс]
-    `}
+    */ //когда-нибудь сделаю
